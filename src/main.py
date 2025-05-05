da20e17788412d6b535c9c52fc8b3104b06fa7d8
Jean1000 <157409715+Jean1000levrai@users.noreply.github.com>
2025-05-05 21:38:35 +0200

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import habit_mgr as hmgr
+import database as db
+
 from kivy.config import Config
 Config.set('graphics', 'width', '360') #1080//3
 Config.set('graphics', 'height', '800')#2400//3
@@ -5,15 +8,46 @@ Config.set('graphics', 'height', '800')#2400//3
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
 
 
 class MainWindow(Screen):
-    pass
+    pass    
 
 class SettingsWindow(Screen):
     pass
 
 class AddHabitWindow(Screen):
+
+    def get_info(self):
+        info = []
+        info.append(self.ids.name.text)
+        info.append(self.ids.descr.text)
+        info.append(self.ids.qu.text)
+        info.append(self.ids.col.text)
+
+        self.ids.name.text = ""
+        self.ids.descr.text = ""
+        self.ids.qu.text = ""
+        self.ids.col.text = ""
+
+        hab = hmgr.HabitYesNo()
+        hab.name = info[0]
+        hab.description = info[1]
+        hab.question = info[2]
+        hab.colour = info[3]
+
+        return hab
+
+    def save_btn(self):
+        try:
+            info = self.get_info()
+            db.add_habit(info)
+            db.print_table()
+        except:
+            print("not a valid input")
+
+class AddHabitPopup(Popup):
     pass
 
 class WindowMgr(ScreenManager):
@@ -24,13 +58,19 @@ class MyMainApp(App):
     def build(self):
         Builder.load_file("ui/main.kv")
         Builder.load_file("ui/settings.kv")
-        Builder.load_file("ui/add_window.kv")
+        Builder.load_file("ui/add_hab.kv")
+        Builder.load_file("ui/habitpopup.kv")
         
         sm = WindowMgr()
         sm.add_widget(MainWindow(name="main"))
         sm.add_widget(SettingsWindow(name="second"))
+        sm.add_widget(AddHabitWindow(name="habYesNo"))
 
         return sm
+    
+    def popup(self):
+        popup = Builder.load_string('AddHabitPopup')()
+        popup.open()
 
 
 if __name__=="__main__":
