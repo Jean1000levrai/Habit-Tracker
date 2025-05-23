6699b3a07318b3fbc077a8d0002c5fd9b1a91339
Jean1000 <157409715+Jean1000levrai@users.noreply.github.com>
2025-05-23 17:39:30 +0200

    Add files via upload

--- src/ui_scripts/addHabitUi.py
+++ src/ui_scripts/addHabitUi.py
@@ -13,6 +13,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 from kivy.properties import ListProperty
 from kivymd.uix.pickers import MDTimePicker
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 
 class AddHabitWindow(Screen):
@@ -159,14 +161,29 @@ class TimePopup(Popup):
         if hasattr(self, 'current_btn') and self.current_btn:
             self.current_btn.text = str(time)
 
+
     def next1(self, instance):
         self.current_btn = instance  # Store the button that was clicked
         popup = MDTimePicker()
         popup.bind(time=self.get_time)
         popup.open() 
+    
+    def day_next(self, instance):
+        if not self.btn_days[instance.text[:-2]][1]:
+            instance.text = instance.text[:-2] + " "
+            self.btn_days[instance.text[:-2]][1] = True
+            
+        else:
+            instance.text = instance.text[:-2] + " "
+            self.btn_days[instance.text[:-2]][1] = False
+            
+        print(self.btn_days[instance.text[:-2]][1])
 
     def next(self):
         self.btn = {}
+        self.btn_days = {}
+
+        # ------time------
         times = self.parent_screen.ids.times.text         # gets the nb of times
 
         container = self.ids.time_select
@@ -176,12 +193,28 @@ class TimePopup(Popup):
             n = int(times)
         except (ValueError, TypeError):
             n = 0
+        if n>6:
+            n=6
         # add the btns
         for i in range(n):
-            self.btn[str(i)] = Button(text="00:00",
+            self.btn[str(i)] = Button(text="00:00:00",
                          size_hint_y = None,
                          height = 40,
                          on_release = self.next1) 
             container.add_widget(self.btn[str(i)])
-     
-    
+        
+        # ------date------
+
+        if self.parent_screen.ids.date_popup.text == "Weekly":
+                    days = ['M', 'T', 'W', 'T H', 'F', 'S', 'S u']
+                    self.container_days = self.ids.days_select
+                    for day in days:
+                        #  uncheck /  check
+                        self.btn_days[day] = [Button(
+                            font_name= "FontAwesome",
+                            text = f"{day} ",
+                            on_release = self.day_next
+                        ),
+                        False]
+                        self.container_days.add_widget(self.btn_days[day][0])
+            
