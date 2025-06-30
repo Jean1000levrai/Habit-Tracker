0fa77878c8b4b2505cc43a5b9a8b18561a51e089
Jean1000 <157409715+Jean1000levrai@users.noreply.github.com>
2025-06-30 09:08:19 +0200

    view to complete hab

---
+++ src/ui_scripts/valib_hab_ui.py
@@ -0,0 +1,16 @@
+from functions import *
+import json
+
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+
+
+class ValidHab(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    
+
+
